abac631c4b0d53047a5c10f0a0384a0fdb44d156
mehdirahimi05 <mehdi.39h@icloud.com>
2026-05-11 08:18:50 +0200

    feat: add Dockerfile and fix pom.xml for Java 21

---
+++ src/main/java/de/fherfurt/FitnessTrackerSystem/Main.java
@@ -0,0 +1,7 @@
+package de.fherfurt.FitnessTrackerSystem;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Fitness Tracker System is running.");
+    }
+}
