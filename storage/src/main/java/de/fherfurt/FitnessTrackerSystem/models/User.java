78857f4fff8846bfb2334b14777582c02e0431e8
mehdirahimi05 <mehdi.39h@icloud.com>
2026-07-12 20:20:20 +0200

    removed k8s to fixed it later

--- storage/src/main/java/de/fherfurt/FitnessTrackerSystem/models/User.java
+++ storage/src/main/java/de/fherfurt/FitnessTrackerSystem/models/User.java
@@ -1,5 +1,6 @@
 package de.fherfurt.FitnessTrackerSystem.models;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.*;
 import lombok.*;
 
@@ -53,6 +54,7 @@ public class User {
      * <p><b>Important:</b> Never store or assign a plain-text password here.
      * Always hash the value before passing it to this field (e.g. via BCrypt).</p>
      */
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     private String passWord;
 
     /**
